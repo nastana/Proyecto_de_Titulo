1fdd76b57465097face46d814d6b059499a679d9
Jeremy Castro <eel_jerhal@hotmail.com>
2022-07-03 19:08:41 -0400

    Se cambia configuración de conexión a BD + se utiliza nueva BD

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,7 +2,9 @@
 #from sqlalchemy import schema
 #from .entities.entity import Session, engine, Base
 #from .entities.exam import Exam, ExamSchema
+from time import strftime,gmtime
 from dis import dis
+from http.client import NON_AUTHORITATIVE_INFORMATION
 from flask import Flask, jsonify, request, flash
 from flask.wrappers import Response
 from flask_mysqldb import MySQL
@@ -18,7 +20,9 @@ from dotenv import load_dotenv
 load_dotenv()
 sys.path.insert(1, 'src/Reidmen Fenics/ipnyb propagation')
 
-from TimeSimTransIsoMatCij2D_test import fmain
+
+#from Reidmen_Fenics.ipnyb_propagation.TimeSimTransIsoMatCij2D_test import fmain
+#from TimeSimTransIsoMatCij2D_test import fmain
 #from 'Reidmen Fenics'.'ipnyb propagation' import TimeSimTransIsoMatCij2D_test
 #from rute import fmain
 #import Reidmen Fenics/ipnyb propagation/TimeSimTransIsoMatCij2D_test.py
@@ -35,20 +39,19 @@ app = Flask(__name__)
 
 
 
-app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+""" app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
 app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
 app.config['MYSQL_PORT'] =  os.getenv('MYSQL_PORT')
 app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
 app.config['MYSQL_DB'] = os.getenv('MYSQL_DB') # proyecto-v01 para win y proyecto_v01 para linux
-app.config['CORS_HEADERS'] = os.getenv('CORS_HEADERS')
+app.config['CORS_HEADERS'] = os.getenv('CORS_HEADERS') """
 
-
-# app.config['MYSQL_HOST'] = 'localhost'
-# app.config['MYSQL_USER'] = 'root'
-# #app.config['MYSQL_PORT'] =  os.getenv('MYSQL_PORT')
-# #app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
-# app.config['MYSQL_DB'] = 'proyecto_v01' # proyecto-v01 para win y proyecto_v01 para linux
-# app.config['CORS_HEADERS'] = 'Content-Type'
+app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_PORT'] =  3306
+app.config['MYSQL_DB'] = 'proyecto_v02' # proyecto-v01 para win y proyecto_v01 para linux
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 mysql = MySQL(app)
 
@@ -63,46 +66,71 @@ app.secret_key = "110997"
 
 @app.route('/')
 def Index():
+    cursor = mysql.connection.cursor()
+    print(cursor)
     return '<h1>Hello world<h1>'
 
 
 @app.route('/Input_data', methods=['POST'])
 def input_data():
-    #print(request.json['status'])
-    #print(request)
-    n_transmitter = request.json['n_transmitter'],
-    n_receiver = request.json['n_receiver'],
-    distance = request.json['distance'],
-    plate_thickness = request.json['plate_thickness'],
+    name_simulation = request.json['sim_name']
+    n_emitters = request.json['n_emitters']
+    n_receivers = request.json['n_receivers']
+    sens_distance = request.json['sens_distance']
+    emitters_pitch = request.json['emitters_pitch']
+    receivers_pitch = request.json['receivers_pitch']
+    sens_edge_margin = request.json['sens_edge_margin']
+    mesh_size = request.json['mesh_size']
+    plate_thickness = request.json['plate_thickness']
+    plate_size = (float(sens_edge_margin) + (int(n_emitters) * 0.8) + ((int(n_emitters) - 1) * float(emitters_pitch)) + float(sens_distance) + (int(n_receivers) * 0.8) + ((int(n_receivers) - 1) * float(receivers_pitch)) + float(sens_edge_margin))
     porosity = request.json['porosity']
-    status = request.json['status']
-    print("Distance", distance)
+    #attenuation = request.json['attenuation']
+    attenuation = "0"
+    status = "0"
+    cod_simulation = strftime("%Y%m%d", gmtime())
+    print(plate_size)
+    print(cod_simulation)
+
     cur = mysql.connection.cursor()
-    cur.execute("INSERT INTO timesimtransisomat_first_step01 (n_transmitter, n_receiver, distance, plate_thickness, porosity, p_status) VALUES (%s,%s,%s,%s,%s,%s)",
-                (n_transmitter, n_receiver, distance, plate_thickness, porosity, status))
+
+    sql = "INSERT INTO simulation (name_simulation, cod_simulation, n_emitter, n_receiver, sensor_distance, emitter_pitch, receiver_pitch, sensor_edge_margin, typical_mesh_size, plate_thickness, plate_size, porosity, attenuation, p_status) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
+    val = (name_simulation, cod_simulation, n_emitters, n_receivers, sens_distance, emitters_pitch, receivers_pitch, sens_edge_margin, mesh_size, plate_thickness, plate_size, porosity, attenuation, status)
+    cur.execute(sql, val)
+
     print(cur.lastrowid)
     last_id = cur.lastrowid
     mysql.connection.commit()
+    cur.close()
+
     flash('data Added successfully')
-    return 'ok'
+    return 'Ok'
 
 
 @app.route('/Load_data', methods=['GET'])
 def load_data():
     data_t = []
     cur = mysql.connection.cursor()
-    cur.execute('SELECT * FROM timesimtransisomat_first_step01')
+    cur.execute('SELECT ID_SIMULATION, NAME_SIMULATION, START_DATETIME, COD_SIMULATION, N_EMITTER, N_RECEIVER, TYPICAL_MESH_SIZE, PLATE_THICKNESS, PLATE_SIZE, POROSITY, ATTENUATION, P_STATUS FROM SIMULATION')
     data = cur.fetchall()
     cur.close()
+
     for doc in data:
+
+        status = returnStatus(doc[11])
+
         data_t.append({
-            'id': doc[0],
-            'n_transmitter': doc[1],
-            'n_receiver': doc[2],
-            'distance': doc[3],
-            'plate_thickness': doc[4],
-            'porosity': float(doc[5]),
-            'p_status' : doc[7]
+            'id_simulation': doc[0],
+            'name_simulation': doc[1],
+            'start_datetime': doc[2],
+            'cod_simulation': doc[3],
+            'n_emitter': doc[4],
+            'n_receiver': doc[5],
+            'typical_mesh_size': doc[6],
+            'plate_thickness': doc[7],
+            'plate_size': doc[8],
+            'porosity': doc[9],
+            'attenuation': doc[10],
+            'p_status': status
         })
     return jsonify(data_t)
 
@@ -113,19 +141,31 @@ def load_data_id(id):
     sub_id = id
     cur = mysql.connection.cursor()
     cur.execute(
-        "SELECT * FROM timesimtransisomat_first_step01 WHERE id = {0}".format(sub_id))
+        "SELECT * FROM SIMULATION WHERE ID_SIMULATION = {0}".format(sub_id))
     data = cur.fetchall()
     cur.close()
     for doc in data:
+
+        status = returnStatus(doc[14])
+
         data_t.append({
-            'id': doc[0],
-            'n_transmitter': doc[1],
-            'n_receiver': doc[2],
-            'distance': doc[3],
-            'plate_thickness': doc[4],
-            'porosity': float(doc[5]),
-            'p_status' : doc[7],
-            'time' : str(doc[8])
+            'id_simulation': doc[0],
+            'cod_simulation': doc[1],
+            'name_simulation': doc[2],
+            'n_emitter': doc[3],
+            'n_receiver': doc[4],
+            'sensor_distance': doc[5],
+            'emitter_pitch': doc[6],
+            'receiver_pitch': doc[7],
+            'sensor_edge_margin': doc[8],
+            'typical_mesh_size': doc[9],
+            'plate_thickness': doc[10],
+            'plate_size': doc[11],
+            'porosity': doc[12],
+            'attenuation': doc[13],
+            'p_status': status,
+            'time': doc[15],
+            'start_datetime': doc[16],
         })
     return jsonify(data_t)
 
@@ -251,12 +291,19 @@ def load_data_id_test(id):
     return 'Funciona'
 
 
-
-
+def returnStatus(status_id):
+    status = "Error"
+    if(status_id == 0):
+        status = "Not Started"
+    elif(status_id == 1):
+        status = "In Progress"
+    elif(status_id == 2):
+        status = "Done"
+    return status
 
 @app.route('/Result')
 def result():
     return 'result'
 
 if __name__ == '__main__':
-    app.run(port=80, debug=False, host="0.0.0.0")
+    app.run(port=3001, debug=False, host="0.0.0.0")
