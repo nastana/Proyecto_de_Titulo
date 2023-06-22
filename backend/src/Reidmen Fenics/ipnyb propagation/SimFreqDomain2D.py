c5e30ff4de486d7189dc626de2e09486a9827692
nastana <seichgeeway@gmail.com>
2023-06-22 22:38:46 +0000

    Reidman Attenuation AND epsilon = omega[f]*tau

--- backend/src/Reidmen Fenics/ipnyb propagation/SimFreqDomain2D.py
+++ backend/src/Reidmen Fenics/ipnyb propagation/SimFreqDomain2D.py
@@ -8,6 +8,8 @@ import pandas as pd
 from ufl import Identity, indices, as_tensor, imag, real
 import math
 
+#import multiprocessing
+
 
 def fmain (n_transmitter, n_receiver, distance, emitter_pitch, receiver_pitch, sensor_edge_margin, typical_mesh_size, plate_thickness, plate_size, sensor_width, porosity, attenuation, id):
     
@@ -16,22 +18,24 @@ def fmain (n_transmitter, n_receiver, distance, emitter_pitch, receiver_pitch, s
     print('Parametros: ', n_transmitter, n_receiver, distance, emitter_pitch, receiver_pitch, sensor_edge_margin, typical_mesh_size, plate_thickness, plate_size, sensor_width, porosity, attenuation, id)
 
     # Obtain the experimental data from .mat file
-    C_mathilde = sio.loadmat(r"src/Reidmen Fenics/ipnyb propagation/Files_mat/C_values_mathilde.mat")
+    C_mathilde = sio.loadmat(r"Files_mat/C_values_mathilde.mat")
 
     # Define the constants
     # The stiffness constants in [GPa] --> [g/mm(\mu sec)^2]
     # are given by the mathilde .mat file of 5%
     #print('WITH ATTENUATION')
-    path = 'src/Reidmen Fenics/ipnyb propagation/Files_mat/attenuation'
+
+    """______________________C AND D VALUES FROM ATTENUATION.CSV______________________"""
+    path = 'Files_mat/attenuation'
     M = pd.read_csv(path + '.csv').values
     # First dimension: porosity
     p = np.arange(0.01, 0.48, 0.01)
-    dDD = 2.03 * (1 - p) + p
+    d = 2.03 * (1 - p) + p
 
-        # Calculate omega
-    #frequency = 1e-3 # frequency value
-    #omega = 2 * np.pi * frequency
-        # Second dimension
+    # Calculate omega
+    frequency = 1e-3 # frequency value
+    omega = 2 * np.pi * frequency
+    # Second dimension
     C22 = M[:, 0] # %=C11
     C66 = M[:, 1]
     C33 = M[:, 2]
@@ -46,11 +50,27 @@ def fmain (n_transmitter, n_receiver, distance, emitter_pitch, receiver_pitch, s
     D55 = M[:, 10]
     D12 = M[:, 11] 
 
-    C11 = C22
-    C13 = C23
-    d = np.reshape(C_mathilde['d'], (30,))*1E-3
-    #C_mathilde.keys()
+    C11 = C22 + D11
+    C13 = C23 + D13
+    C66 = C66 + D66
+    C33 = C33 + D33
+    C55 = C55 + D55
+    C12 = C12 + D12
 
+
+    """______________________C VALUES FROM MATHILDE______________________"""
+    C11 = np.reshape(C_mathilde['C11'], (30,))*1E-3
+    C12 = np.reshape(C_mathilde['C12'], (30,))*1E-3
+    C13 = np.reshape(C_mathilde['C13'], (30,))*1E-3
+    C33 = np.reshape(C_mathilde['C33'], (30,))*1E-3
+    C55 = np.reshape(C_mathilde['C55'], (30,))*1E-3
+    C66 = np.reshape(C_mathilde['C66'], (30,))*1E-3
+
+    d = np.reshape(C_mathilde['d'], (30,))*1E-3
+    C_mathilde.keys()
+
+    """______________________PARAMS______________________"""
+    """
     # Rectangle geometry limits
     zlim=(2*sensor_edge_margin) + (n_transmitter*emitter_pitch) + distance + (n_receiver*receiver_pitch)
     ylim = plate_thickness
@@ -84,7 +104,33 @@ def fmain (n_transmitter, n_receiver, distance, emitter_pitch, receiver_pitch, s
     # Parameters of Source definition
     eps = DOLFIN_EPS
     width = sensor_width #antes era 0.5 sens_width
+    
+    """# Rectangle geometry limits
+    zlim, ylim = 70., 2.8
+    # Porosity level from Mathilde data 
+    # this data starts from 1% porosity values!
+    por = porosity# the porosity level is por+1
+    # Define domain size 
+    size = 400
+    # generate f and create mesh
+    domain = Rectangle(Point(0., 0.), Point(zlim, ylim))
+    mesh = generate_mesh(domain, size)
+    # Compute the minimum height diameter
+    print("Minimum height of element [mm]: ", mesh.hmin())
+    #File("Domains/2DMesh.pvd") << mesh
 
+    # Define source locations
+    # Using notation consistent with the 3D-case
+    nsous = 8
+    zsous, ysous = [n for n in range(10, 25, 2)], nsous*[ylim,]
+    # Position of sensor to obtain the force!
+    nsens = 50# Number of sensors separated at 0.4081 mm
+    zsens, ysens = np.arange(35, 55, step=0.4), nsens*[ylim,]
+    # Parameters of Source definition
+    eps = DOLFIN_EPS
+    width = 0.5
+
+    """______________________DOMAINS______________________"""
     # Define domain for each source
     class DomSource_1(SubDomain):
         def inside(self, x, on_boundary):
@@ -134,24 +180,29 @@ def fmain (n_transmitter, n_receiver, distance, emitter_pitch, receiver_pitch, s
                     abs(x[1] - ysous[7]) < width + eps and
                    on_boundary)
     
-
     # Define source expression
     class Source(UserExpression):
         def __init__(self, freq, freq_0,
-                    sig_freq, degree=1, **kwargs):
+                    sig_freq, osc, 
+                    degree=1, **kwargs):
             super().__init__(**kwargs)
             # initialize atributes
             self.freq, self.freq_0 = freq, freq_0
             self.sig_freq = sig_freq
+            self.osc = osc
         
         def eval(self, values, x):
             ## Case using Fourier transform with -2*pi on the oscilation
             t_0 = 5.0 # [\mu s]
             sig2 = pow(self.sig_freq,2)
             pi2 = pow(pi,2)
-            factor = sqrt(pi*sig2/2)*sin(-2*pi*self.freq*self.freq_0)
             sum_freq = (self.freq_0 + self.freq)
             dif_freq = (self.freq_0 - self.freq)
+            # Type of oscillation to consider
+            if self.osc == "cos":
+                factor = sqrt(pi*sig2/2)*cos(-2*pi*self.freq*self.freq_0)
+            else:
+                factor = sqrt(pi*sig2/2)*sin(-2*pi*self.freq*self.freq_0)
             # Obtaining left side and right sides
             num_ls = exp(-2*sig2*pi2*pow(sum_freq,2))
             num_rs = exp(-2*sig2*pi2*pow(dif_freq,2))
@@ -160,6 +211,28 @@ def fmain (n_transmitter, n_receiver, distance, emitter_pitch, receiver_pitch, s
                 
         def value_shape(self):
             return (2,)
+
+    class Attenuation(UserExpression):
+        def __init__(self, epsilon, zlim, ylim, degree=1):
+            super().__init__()
+            # Define constant factor for epsilon
+            self._eps = epsilon
+            # Define limits of geometry for damping
+            self._ylim = ylim
+            self._zlim = zlim
+            
+        def eval(self, values, x):
+            # If we are in the main geometry
+            if x[0] > 0.0 or x[0] < self._zlim:
+                values[0] = self._eps
+            # If we are outside the interest zone
+            else:
+                if x[0] < 0.0 + DOLFIN_EPS:
+                    values[0] = self._eps*(1+np.abs(x[0]))
+                else:
+                    values[0] = self._eps*(1+np.abs(x[0]-self._zlim))
+        def value_shape(self):
+            return ()
     
     print("Number of Cells; {0}, of Vertice: {1}".format(mesh.num_cells(),
                                                     mesh.num_vertices()))
@@ -192,14 +265,19 @@ def fmain (n_transmitter, n_receiver, distance, emitter_pitch, receiver_pitch, s
     dx = dx(domain=mesh)
     ds = ds(domain=mesh, subdomain_data=boundaries)
 
-    # Define function spaces and boundary conditions
+    # Define mixed function space with boundary conditions
     pdim = 1
-    VV = VectorFunctionSpace(mesh, 'CG', pdim)
+    V = VectorElement("CG", mesh.ufl_cell(), pdim)
+    V_element = MixedElement([V, V])
+    W = FunctionSpace(mesh, V_element)
+    # Define a constant function space for parameters
+    P = FunctionSpace(mesh, 'DG', 0)
 
     # Define boundaries over the function space
     # In the variational form are defined the Neumann conditions
     left_jit = "on_boundary && near(x[0], 0.)"
-    bc_domain = DirichletBC(VV, Constant((0.,0.)), left_jit)
+    bc_0 = DirichletBC(W.sub(0), Constant((0.,0.)), left_jit)
+    bc_1 = DirichletBC(W.sub(1), Constant((0.,0.)), left_jit)
 
 
     # Define kronecker delta in 2D and indices
@@ -207,9 +285,9 @@ def fmain (n_transmitter, n_receiver, distance, emitter_pitch, receiver_pitch, s
     i,j,k,l = indices(4)
 
     # Define strain tensor
-    def epsilon(u): 
+    def strain(u): 
         return as_tensor(0.5*(u[i].dx(j)+u[j].dx(i)),
-                        (i,j))
+                    (i,j))
 
     # Define stiffness tensor C_{i,j,k,l} transverse isotropic
     def VoigtToTensor(A):
@@ -249,7 +327,7 @@ def fmain (n_transmitter, n_receiver, distance, emitter_pitch, receiver_pitch, s
 
     """_____________________D VALUES_____________________"""
     # We take the standard density
-    rho = dDD[por] # [g/cm^3] --> [g/(mm)^3]
+    rho = d[por] # [g/cm^3] --> [g/(mm)^3]
     # Define the Voigt matrix representing the tensor
     # Here, the 3-axis is the z-axis, and
     # 1-axis in the y-axis or x-axis by the simmetry
@@ -263,116 +341,131 @@ def fmain (n_transmitter, n_receiver, distance, emitter_pitch, receiver_pitch, s
     
     """_____________________FUNCTIONS_____________________"""
     # Define stress tensor
-    def sigma(u,omega=''):
-        if omega:
-            return as_tensor(omega*D[i,j,k,l]*epsilon(u)[k,l], 
-                        (i,j))
-        return as_tensor(C[i,j,k,l]*epsilon(u)[k,l], 
-                        (i,j))
+    def sigma(u):
+        return as_tensor(C[i,j,k,l]*strain(u)[k,l], (i,j))
 
     # Define the three main blocks in variational forms
     def o_block(u, v, omega):
         return -rho*pow(omega,2)*inner(u, v)*dx
         #return -rho*pow(omega,2)*inner(grad(imag(u)), grad(imag(v))) * dx
 
-    def A_block(u, v,omega=''):
-        if omega:
-            return inner(sigma(u,omega), grad(v))*dx
+    def eps_block(u, v, f, damping):
+        return damping*f*inner(u, v)*dx
+
+    def A_block(u, v):
         return inner(sigma(u), grad(v))*dx
-        #return  inner(grad(real(u)), grad(real(v)))
 
     def b_block(source, v, bdry_id):
         return dot(source, v)*ds(bdry_id)
 
-    # Frequency array to consider
-    freqs = np.arange(0., 2., step=20/2048)# ~ [MHz], step=20/2048
-    omegas = 2*pi*freqs
-    # print(omegas)
-    # Define number of frequencies
-    nfreq = freqs.shape[0]
-    # Define sensors arrays
-    sol_sensors_z = np.zeros((nsens, nfreq, nsous))
-    sol_sensors_y = np.zeros((nsens, nfreq, nsous))
-
     """_____________________INFO_____________________"""
     # Print number of point at the boundary
-    print("Total number of points at the boundary", 
-        len(bc_domain.get_boundary_values()))
+    #print("Total number of points at the boundary", 
+    #    len(bc_domain.get_boundary_values()))
 
     # Check the points where the force is applied
-    mesh_points = SubsetIterator(boundaries, 28)
-    print("Midpoints where the force is applied")
+    #mesh_points = SubsetIterator(boundaries, 28)
+    #print("Midpoints where the force is applied")
     for ii in range(nsous):
         idx = int(ii + 21)
         count = sum(1 for _ in SubsetIterator(boundaries,idx))
-        print("Facet marked as %d have %d points" % (idx, count))
-    #for data in mesh_points:
-    #    print("Points at boundary 28, ({0},{1}) ".format(
-    #        data.midpoint().x(), data.midpoint().y()))
+    print("Facet marked as %d have %d points" % (idx, count))
     
     # Check if the points a the left have been taken.
-    print("Points at boundary left: ", len(bc_domain.get_boundary_values()))
+    print("Points at boundary left(): ", 
+        len(bc_0.get_boundary_values()))
+
+    """________________START________________"""
+    # Frequency array to consider
+    freqs = np.arange(0., 2., step=20/2048)# ~ [MHz], step=20/2048
+    omegas = 2*pi*freqs
+
+    # Define number of frequencies
+    nfreq = freqs.shape[0]
 
     ## Iterate of freq. and solution saving for a point sensor
     # Define trial and test functions
-    u = TrialFunction(VV)
-    v = TestFunction(VV)
-
-    # Shape and dimension
-    d = u.geometric_dimension()
-    u_shape = grad(u).ufl_shape
-    print("Geometric dimension of u: {0}".format(d))
-    print("UFL shape of grad(u): {0}".format(u_shape))
+    (u_r, u_i) = TrialFunctions(W)
+    (v_r, v_i) = TestFunctions(W)
 
     # list all boundaries
-    bcs = [bc_domain]
+    bcs = [bc_0, bc_1]
 
-    # Define name solutions for saving with File
-    #filename = "SimP"+str(por+1)+"TransIso"+str(ylim)+ \
-    #           "M"+str(size)+"Freq.pvd"
-    #filepvd ="Results/"+filename
-    #vtk_u = File(filepvd)
+    # Define sensors arrays
+    # Define sensors arrays for real and imaginary parts
+    solR_sensors_z = np.zeros((nsens, nfreq, nsous))
+    solR_sensors_y = np.zeros((nsens, nfreq, nsous))
+    solI_sensors_z = np.zeros((nsens, nfreq, nsous))
+    solI_sensors_y = np.zeros((nsens, nfreq, nsous))
         
-    # Iteration over each frequency
-    for freq_i in range(nfreq):
-        ## Define Neumann boundary condition for source 
-        freq, freq_0 = float(freqs[freq_i]), 0.5#freq_0 ~ 1 [MHz]
-        print("THE FREQ:{} | THE FREQ 0:{}".format(freq,freq_0))
-        # Define general variance
-        sig_freq = 0.7 # sig_freq ~ 0.7 [Mhz]
-        source_exp = Source(freq=freq, freq_0=freq_0,
-                            sig_freq=sig_freq, degree=1)
-        # Interpolate source over domain
-        source = interpolate(source_exp, VV)
+    # Define parameter for the imaginary part
+    # epsilon = 0.5E-3 # TEST!
 
-        # Variational forms
-        A_lhs = o_block(u,v,omegas[freq_i]) + A_block(u,v)
-        # print("A_lhs",'-'*8,A_lhs)
-        A = assemble(A_lhs)
+    tau0 = 0.02
+    B = 0.133;   # = 0.04 / 0.3
+
+    tau = tau0 + B * por
+    # Iteration over each frequency
+    for idx_i in range(nfreq):
+
+        epsilon = omegas[idx_i] * tau
+        ## Define Neumann boundary condition for source 
+        freq, freq_0 = float(freqs[idx_i]), 0.5 # ~ 0.5 [MHz]
+        # Define general variance
+        sig_freq = 0.7 # sig_time ~ 0.7 [Mhz] 
+        exp_R = Source(freq=freq, freq_0=freq_0,
+                    sig_freq=sig_freq, osc="cos",
+                    degree=1)
+        exp_I = Source(freq=freq, freq_0=freq_0,
+                    sig_freq=sig_freq, osc="sin",
+                    degree=1)
+        # Interpolate source over domain
+        src_R = interpolate(exp_R, W.sub(0).collapse())
+        src_I = interpolate(exp_I, W.sub(1).collapse())
+        
+        # Define damping and interpolate it
+        exp_att = Attenuation(epsilon=epsilon, 
+                            zlim=zlim, ylim=ylim,
+                            degree=1)
+        damping = interpolate(exp_att, P)
+        # Variational forms (real and imaginary parts
+        Ar_lhs = o_block(u_r,v_r,omegas[idx_i]) + \
+                eps_block(u_i,v_r,omegas[idx_i],damping) + \
+                A_block(u_r,v_r)
+        Ai_lhs = o_block(u_i,v_i,omegas[idx_i]) - \
+                eps_block(u_r,v_i,omegas[idx_i],damping) + \
+                A_block(u_i,v_i)             
+
+        A = assemble(Ar_lhs + Ai_lhs)
         # Apply source sequentially 
         for sous_j in range(nsous):   
             # Define function-solution
-            u_sol = Function(VV)
-            # Compute bounda ry layer
+            u_sol = Function(W)
+            #u_rsol = Function(W.sub(0).collapse())
+            #u_isol = Function(W.sub(1).collapse())
+            # Compute boundary layer
             bdry_id = int(21 + sous_j)
-            b_rhs = b_block(source, v, bdry_id)
+            b_rhs = b_block(src_R, v_r, bdry_id) + \
+                    b_block(src_I, v_i, bdry_id)
             
             # Assemble of matrices
             b = assemble(b_rhs)
             # Apply boundary conditions
             [bc.apply(A,b) for bc in bcs]
-            # DEBUG!
-            #print("Value rhs: ", np.linalg.norm(np.array(b), ord=2))
+            
             # Solve the variational problem
             solve(A, u_sol.vector(), b)
-                
+            # Assign real and imaginary parts
+            (u_rsol, u_isol)  = u_sol.split(True)
             # Compute data at the sensors and save it as array
             for sens_k in range(nsens):
                 # Obtain point value of solution u
                 sensor_point = Point(np.array((zsens[sens_k], ysens[sens_k])))
                 # Save data values at the sensor location
-                sol_sensors_z[sens_k, freq_i, sous_j] = u_sol(sensor_point)[0]
-                sol_sensors_y[sens_k, freq_i, sous_j] = u_sol(sensor_point)[1]
+                solR_sensors_z[sens_k, idx_i, sous_j] = u_rsol(sensor_point)[0]
+                solR_sensors_y[sens_k, idx_i, sous_j] = u_rsol(sensor_point)[1]
+                solI_sensors_z[sens_k, idx_i, sous_j] = u_isol(sensor_point)[0]
+                solI_sensors_y[sens_k, idx_i, sous_j] = u_isol(sensor_point)[1]
                 
             if sous_j == 0:
                 # Save vtk solution
@@ -382,17 +475,19 @@ def fmain (n_transmitter, n_receiver, distance, emitter_pitch, receiver_pitch, s
                     u_sol(sensor_point)[1], freq))
                 #print("Value rhs", np.linalg.norm(b, ord=2))
 
+    # create dictionary with variables to save
     savedic = {'zlim': zlim, 'ylim': ylim, 'nsous': nsous,
-          'zsous': zsous, 'ysous': ysous, 'nsens': nsens,
-          'zsens': zsens, 'ysens': ysens, 'nfreq': nfreq,
-          'freqs': freqs, 'omegas': omegas,
-          'sol_sensors_z': sol_sensors_z, 'sol_sensors_y': sol_sensors_y}
-    filename1 = 'src/Reidmen Fenics/ipnyb propagation/Files_mat/FreqSimp'+str(por+1)+'TransIsoW'+ str(ylim)+'M'+str(size) + str(id) + '.mat'
-    filename = 'FreqSimp'+str(por+1)+'TransIsoW'+ str(ylim)+'M'+str(size) + str(id) + '.mat'
+        'zsous': zsous, 'ysous': ysous, 'nsens': nsens,
+        'zsens': zsens, 'ysens': ysens, 'nfreq': nfreq,
+        'freqs': freqs, 'omegas': omegas,
+        'solR_sensors_z': solR_sensors_z, 'solR_sensors_y': solR_sensors_y,
+        'solI_sensors_z': solI_sensors_z, 'solI_sensors_y': solI_sensors_y}
+    filename1 = 'Files_mat/FreqSimp'+"POR"+str(por+1)+'TransIsoW'+ str(ylim)+'M'+str(size) + str(id) + '.mat'
+    filename = 'FreqSimp'+"POR"+str(por+1)+'TransIsoW'+ str(ylim)+'M'+str(size) + str(id) + '.mat'
     sio.savemat(filename1, savedic, appendmat=True)
 
+    print(filename1+"\n"+filename)
     ## Zona de Pruebas de tiempo ##
-
     Hora_final = datetime.now()
     print(Hora_final)
     tiempo_ejecucion = Hora_final - Hora_inicio
