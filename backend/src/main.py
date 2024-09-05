88849de83d97fd0624cd8383f196e0820aa8d23f
ManuelB <manuel.barrera@alumnos.uv.cl>
2024-09-05 02:29:58 -0400

    se soluciona error de conexion a la base de datos

--- backend/src/main.py
+++ backend/src/main.py
@@ -37,6 +37,8 @@ app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
 app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
 app.config['CORS_HEADERS'] = os.getenv('CORS_HEADERS')
 
+app.config['SQLALCHEMY_POOL_RECYCLE'] = 28800
+
 mysql = MySQL(app)
 
 CORS(app)
